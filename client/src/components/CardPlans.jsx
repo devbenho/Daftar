18a6de62c641e09847eb57e84333dcb5d39560cb
Muhammed Hamdy <modclover7@gmail.com>
2023-04-06 00:54:27 +0200

    change components name

---
+++ client/src/components/CardPlans.jsx
@@ -0,0 +1,9 @@
+
+export const CardPlane = (props) => {
+  return (
+    <div className={`bg-brand-${props.colorNum} h-[${props.height}px] w-[${props.width}px] rounded-xl`}>
+
+    </div>
+
+  );
+}
