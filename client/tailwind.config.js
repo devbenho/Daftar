7aeb0b3b7c9534f969c37a4be32214898c24a648
Muhammad Banhawy <muhammadbanhawy@gmail.com>
2023-04-05 20:44:43 +0200

    Theme added

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      brand:{
+        50:'#E9F7FA',
+        75:'#A6DFE8',
+        100: '#82D1DF',
+        200: '#4CBDD1',
+        300:'#27B0C8',
+        400: '#1B7B8C',
+        500: '#186B7A',
+      }
+    },
   },
   plugins: [],
 }
