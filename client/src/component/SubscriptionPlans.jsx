98666428591e8e03429587175f1c3beafb74fceb
bassilharoun <bassilharoun123@gmail.com>
2023-04-05 23:38:07 +0200

    init first sub

--- client/src/component/SubscriptionPlans.jsx
+++ client/src/component/SubscriptionPlans.jsx
@@ -1,3 +1,10 @@
+export const SubscriptionsPlans = () => {
+    return (
+        <>
+            <div className="bg-brand-400 h-[570px] w-[300px]">
 
+            </div>
+        </>
+    );
+}
 
-// TODO BASSIL
