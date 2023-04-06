0260660afeb890aa15e32790292e2151bc0c369b
motaz-14 <82406816+motaz-14@users.noreply.github.com>
2023-04-06 06:23:56 +0200

    LogIn & SignUp

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,9 @@
-import React from "react";
 // import HeaderLogo from "./HeaderLogo";
 // import UserProfileName from "./component/UserInfo";
 // import { FaUserCircle } from "react-icons/fa";
 // import HeaderPic from "./Header-pic";
-import { Header } from "./component/Header";
+// import { Header } from "./component/Header";
 
-function App() {
-  return (
-   <Header/>
-  );
-}
+function App() {}
 
 export default App;
