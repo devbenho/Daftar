68a83028c30fe0dac7fabfd9db6a14141cac423e
motaz-14 <82406816+motaz-14@users.noreply.github.com>
2023-04-06 08:14:09 +0200

    UPDATE FOOTER

--- client/src/components/FooterApp.jsx
+++ client/src/components/FooterApp.jsx
@@ -1,38 +1,40 @@
 import HeaderLogo from "../assets/svg/HeaderLogo"
 import { FiArrowUpCircle } from 'react-icons/fi';
-import { GrLocation } from 'react-icons/gr';
+import { CiLocationOn } from 'react-icons/ci';
 export const FooterApp = () =>{
     return(
         <>
-        <footer className="grid grid-cols-5 w-full h-80 bg-brand-500 px-6 py-6">
-        <div>
+        <footer className="grid grid-cols-5 w-full h-80 bg-brand-500 px-6 py-10">
+        <div className="flex justify-center">
             <HeaderLogo/>
         </div>
-        <div className="flex flex-col">
-                <div> <p className="text-4xl text-brand-50 text-start  mb-10 "><span></span> Nasr City, Cairo Egypt</p></div>
-            <div><p className="text-3xl text-brand-50 text-start underline mb-10 "><a href="/number">(111) 111-1111</a></p></div>
-            <div><a href="/gmail" className="text-3xl text-brand-50  text-start underline mb-10 ">contact@daftar.com</a></div>
+        <div className="flex flex-col text-brand-50">
+                <div> <p className="text-[24px]  text-start  mb-10 flex justify-center items-center gap-1"><CiLocationOn/> Nasr City, Cairo Egypt</p></div>
+            <div><p className="text-1xl text-start underline mb-10 relative left-9"><a href="/number">(111) 111-1111</a></p></div>
+            <div><a href="/gmail" className="text-1xl text-start underline mb-10 relative left-9">contact@daftar.com</a></div>
         </div>
-        <div className="ml-3">
-        <ul className="text-3xl text-brand-50 text-center mt-3">
-                <li className="m-1"><a href="/About us">About us</a></li>
-                <li className="m-1"><a href="/Developers">Developers</a></li>
-                <li className="m-1"><a href="/Courses">Courses</a></li>
-                <li className="m-1"><a href="/Partners">Partners</a></li>
+        <div className="ml-8">
+        <ul className="text-[16px] text-brand-50 text-start mt-3">
+                <li className="m-3 hover:text-[#A6AEBB] transition-all "><a href="/About us">About us</a></li>
+                <li className="m-3 hover:text-[#A6AEBB] transition-all"><a href="/Developers">Developers</a></li>
+                <li className="m-3 hover:text-[#A6AEBB] transition-all"><a href="/Courses">Courses</a></li>
+                <li className="m-3 hover:text-[#A6AEBB] transition-all"><a href="/Partners">Partners</a></li>
         </ul>
         </div>
         <div className="ml-5">
-        <ul className="text-3xl text-brand-50 text-center mt-3">
-        <li className="m-1"><a href="Contact us">Contact us</a></li>
-                <li className="m-1"><a href="/Facebook">Facebook</a></li>
-                <li className="m-1"><a href="/Twitter">Twitter</a></li>
-                <li className="m-1"><a href="Instagram">Instagram</a></li>
+        <ul className="text-[16px] text-brand-50 text-start mt-3">
+        <li className="m-3 hover:text-[#A6AEBB] transition-all "><a href="Contact us">Contact us</a></li>
+                <li className="m-3 hover:text-[#A6AEBB] transition-all"><a href="/Facebook">Facebook</a></li>
+                <li className="m-3 hover:text-[#A6AEBB] transition-all"><a href="/Twitter">Twitter</a></li>
+                <li className="m-3 hover:text-[#A6AEBB] transition-all"><a href="Instagram">Instagram</a></li>
         </ul>
-        <p className="text-sm text-brand-50 text-star   mt-11">© 2023 Daftar Platform. All rights reserved.</p>
+        <p className="text-sm text-brand-50 text-star mt-11">© 2023 Daftar Platform. All rights reserved.</p>
         </div>
-    <button className="flex justify-center text-brand-50 text-7xl mt-20 hover:text-[#FFD5D0]">
+        <div className="flex justify-center text-brand-50 text-7xl">
+        <button className="hover:text-[#FFD5D0] h-fit mt-8">
             <FiArrowUpCircle/>
         </button>
+        </div>
         </footer>
         </>
     )
